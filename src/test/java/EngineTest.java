1d0ac01efc02674d3e8349b80de3e8ee0f8fc724
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-10-28 22:36:03 -0700

    O can win on the top row.

--- src/test/java/EngineTest.java
+++ src/test/java/EngineTest.java
@@ -5,19 +5,24 @@ import static org.junit.Assert.assertThat;
 
 public class EngineTest {
 
-    enum Winner {X}
+    enum Winner {O, X}
 
     @Test
     public void testXWinsTopRow() throws Exception {
         assertThat(winner("A1,B1,A2,B2,A3"), is(Winner.X));
     }
 
+    @Test
+    public void testOWinsTopRow() throws Exception {
+        assertThat(winner("B1,A1,B2,A2,C3,A3"), is(Winner.O));
+    }
+
     private Winner winner(String game) {
         if (didXWin(game)) return Winner.X;
-        return null;
+        return Winner.O;
     }
 
     private boolean didXWin(String game) {
-        return game.contains("A");
+        return game.startsWith("A");
     }
 }
