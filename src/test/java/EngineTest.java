ec7fbd77c772977bc70beb7e8303b82ec93ada8f
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-10-28 22:33:42 -0700

    Refactor.  Does this assume too much?

--- src/test/java/EngineTest.java
+++ src/test/java/EngineTest.java
@@ -5,8 +5,19 @@ import static org.junit.Assert.assertThat;
 
 public class EngineTest {
 
+    enum Winner {X}
+
     @Test
     public void testXWinsTopRow() throws Exception {
-        assertThat("A1,B1,A2,B2,A3".contains("A"), is(true));
+        assertThat(winner("A1,B1,A2,B2,A3"), is(Winner.X));
+    }
+
+    private Winner winner(String game) {
+        if (didXWin(game)) return Winner.X;
+        return null;
+    }
+
+    private boolean didXWin(String game) {
+        return game.contains("A");
     }
 }
