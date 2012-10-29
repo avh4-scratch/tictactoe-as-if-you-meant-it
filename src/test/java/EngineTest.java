1ab6fae4b246794df2a06399fd8b91a53628c745
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-10-28 22:44:05 -0700

    X can win games of any length

--- src/test/java/EngineTest.java
+++ src/test/java/EngineTest.java
@@ -22,13 +22,18 @@ public class EngineTest {
         assertThat(winner("A1,B1,A2,B2,C3,B3"), is(Winner.O));
     }
 
+    @Test
+    public void testXWinsGameWith7Moves() throws Exception {
+        assertThat(winner("A1,B1,A2,B2,C1,C2,A3"), is(Winner.X));
+    }
+
     private Winner winner(String game) {
         if (didXWin(game)) return Winner.X;
         return Winner.O;
     }
 
     private boolean didXWin(String game) {
-        return numberOfMoves(game) == 5;
+        return numberOfMoves(game) % 2 == 1;
     }
 
     private int numberOfMoves(String game) {
